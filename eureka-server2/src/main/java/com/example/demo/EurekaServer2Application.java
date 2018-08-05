e61cca6759738fcecb56bf4fa474c25a0f671122
zmOnly1 <406312944@qq.com>
2018-08-06 01:23:22 +0800

    eureka server

---
+++ eureka-server2/src/main/java/com/example/demo/EurekaServer2Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer2Application.class, args);
+    }
+}
