b3c8cc9a55a8e1dd09b9e53cf9d2fdd08344efef
zmOnly1 <406312944@qq.com>
2018-08-19 00:34:36 +0800

    remove dep

--- guice/src/main/java/com/example/guice/PrintStreamWriter.java
+++ guice/src/main/java/com/example/guice/PrintStreamWriter.java
@@ -1,6 +1,5 @@
 package com.example.guice;
 
-import javax.inject.Inject;
 import java.io.*;
 
 /**
@@ -10,7 +9,6 @@ public class PrintStreamWriter implements MyDestination {
 
     private PrintStream destination;
 
-    @Inject
     public PrintStreamWriter(PrintStream destination) {
         this.destination = destination;
     }
